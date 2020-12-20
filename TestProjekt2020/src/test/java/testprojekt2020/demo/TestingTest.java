8d20d92e032d9d4131680c275a5600a866b93557
Thomas Simonsen <thom622g@stud.kea.dk>
2020-12-20 09:23:21 +0100

    fjernet test projekt

--- TestProjekt2020/src/test/java/testprojekt2020/demo/TestingTest.java
+++
@@ -1,8 +0,0 @@
-package testprojekt2020.demo;
-
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-
-import static org.junit.jupiter.api.Assertions.*;
-
